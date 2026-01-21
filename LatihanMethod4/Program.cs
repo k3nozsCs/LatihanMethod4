f5d379660e9a9b17115152f6a852f271bb1541d2
k3nozsCs <tarakenzo3@gmail.com>
2026-01-21 08:40:17 +0700

    method

--- LatihanMethod4/Program.cs
+++ LatihanMethod4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//total beli
+decimal pembelian (decimal harga, int jumlah)
+{
+    decimal result = harga * jumlah;
+    return result;
+}
+
+//pajak
+decimal pajak (decimal pembelian, decimal pajak = 0.11m)
+{
+    decimal result = pembelian * pajak;
+    return result;
+}
