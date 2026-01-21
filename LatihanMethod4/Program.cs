c29bc80a1444ecc1c219366b0aab47e4693fe722
k3nozsCs <tarakenzo3@gmail.com>
2026-01-21 08:59:08 +0700

    finishing

--- LatihanMethod4/Program.cs
+++ LatihanMethod4/Program.cs
@@ -1,13 +1,31 @@
-﻿//total beli
-decimal pembelian (decimal harga, int jumlah)
+﻿int harga, jumlah;
+decimal totalBeli, totalPajak, totalBayar;
+
+//total beli
+decimal pembelian (int hrgitm, int jmlitm)
 {
-    decimal result = harga * jumlah;
-    return result;
+    return hrgitm * jmlitm;
 }
 
 //pajak
-decimal pajak (decimal pembelian, decimal pajak = 0.11m)
+decimal pajak (decimal ttlbeli, decimal pajak = 0.11m)
 {
-    decimal result = pembelian * pajak;
-    return result;
+    return ttlbeli * pajak;
 }
+
+//input
+Console.WriteLine("-Hitung Pajak-\n");
+
+Console.Write("Jumlah Item : ");
+jumlah = int.Parse(Console.ReadLine());
+
+Console.Write("Harga Item : Rp.");
+harga = int.Parse(Console.ReadLine());
+
+//hitung
+totalBeli = pembelian(harga, jumlah);
+totalPajak = pajak(totalBeli);
+totalBayar = totalBeli + totalPajak;
+
+//output
+Console.WriteLine($"Total yang Harus DIbayarkan : Rp.{totalBayar.ToString("N0")}");
